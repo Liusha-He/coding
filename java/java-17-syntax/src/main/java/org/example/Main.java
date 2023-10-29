ca8afe4165bd3f05e6012d3150ebc5e82e31147d
lhe <liusha.8852@yahoo.com>
2023-10-29 16:55:49 +0000

    merge kotlin and java

--- java/java-17-syntax/src/main/java/org/example/Main.java
+++ java/java-17-syntax/src/main/java/org/example/Main.java
@@ -2,11 +2,34 @@ package org.example;
 
 import org.example.helpers.Calculator;
 
+import java.util.Map;
+import static java.util.Map.entry;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        long startTime = System.currentTimeMillis();
+
+        Map<Integer, Integer> map = Map.ofEntries(
+                entry(5, 7),
+                entry(8, 10),
+                entry(13, 3),
+                entry(10, 14)
+        );
 
         Calculator<Integer> calc = new Calculator<>();
-        System.out.printf("%d + %d = %.2f", 5, 7, calc.add(5, 7));
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Integer x = entry.getKey();
+            Integer y = entry.getValue();
+
+            System.out.printf("%d + %d = %.2f\n", x, y, calc.add(x, y));
+            System.out.printf("%d - %d = %.2f\n", x, y, calc.minus(x, y));
+            System.out.printf("%d x %d = %.2f\n", x, y, calc.mutiply(x, y));
+            System.out.printf("%d / %d = %.2f\n", x, y, calc.divide(x, y));
+        }
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        System.out.printf("the code execution time = %d nanoseconds.", executionTime);
     }
 }
