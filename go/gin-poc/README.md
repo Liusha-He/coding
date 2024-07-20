c7efe87550302f8c40e3e04d05c28fdc8fbe86b1
liusha <liusha.8852@yahoo.com>
2024-07-20 17:06:58 +0100

    add more gin poc code

---
+++ go/gin-poc/README.md
@@ -0,0 +1,17 @@
+# Simple Bank Service in Gin and Postgres
+
+## 1. Introduction
+
+
+
+### DataBase
+
+![DataBase Design](../../../.config/JetBrains/IdeaIC2024.1/scratches/.github/assets/db_schema.png)
+
+## 2. Setup
+
+
+
+## 3. Contribute
+
+
---
+++ go/gin-poc/src/service/accounts.go
@@ -0,0 +1,126 @@
+package service
+
+import (
+	"errors"
+	"net/http"
+	"simple-bank/src/auth"
+	"simple-bank/src/dao"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR GBP CNY"`
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+type listAccountsRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+// Create Account 	godoc
+// @Summary 		create account
+// @Schemes 		http
+// @Description 	Takes an account json and store in DB, Returned saved json.
+// @Tags 			services
+// @Produce 		json
+// @Param 			account  body	createAccountRequest true  "account json"
+// @Success 		200 {object} dao.Account
+// @Router 			/api/v1/services/accounts [post]
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*auth.Payload)
+	arg := dao.CreateAccountParams{
+		Owner:    authPayload.Username,
+		Currency: req.Currency,
+		Balance:  0.00,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, account)
+}
+
+// GetAccountByID 	godoc
+// @Summary 		get account by id
+// @Schemes 		http
+// @Description 	Takes an account id with path, Returned account info json.
+// @Tags 			services
+// @Produce 		json
+// @Param 			id path string true "search by id"
+// @Success 		200	{object} dao.Account
+// @Router 			/api/v1/services/accounts/{id} [get]
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*auth.Payload)
+	if account.Owner != authPayload.Username {
+		err := errors.New("account doesn't belong to authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
+
+// ListAccounts godoc
+// @Summary 		Get account list
+// @Schemes 		http
+// @Description		Responds with the list of accounts belong to the authenticated user.
+// @Tags 			services
+// @Produce 		json
+// @Success 		200	{array} dao.Account
+// @Router 			/api/v1/services/accounts [get]
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req listAccountsRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*auth.Payload)
+	arg := dao.ListAccountsParams{
+		Owner:  authPayload.Username,
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+}
